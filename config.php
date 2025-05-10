2c52a16292958067413c550a870bebd821b3b2f3
米诺 <1213235865@qq.com>
2025-05-10 12:16:52 +0800

    Update config.php

--- config.php
+++ config.php
@@ -12,7 +12,7 @@ return array (
   'player_api_prefix' => 'api.php?url=',
   'background_image_url' => 'https://pic1.imgdb.cn/item/6812a7ae58cb8da5c8d5cbab.png',
   'source_count' => '6',
-  'baiapi_key' => '',
+  'baiapi_key' => 'bb5256a782551dc769f0751801e8866d',
   'disclaimers' => '本站所有内容均来自互联网，本站不会保存、复制或传播任何视频文件，也不对本站上的任何内容负法律责任。如果本站部分内容侵犯您的版权请告知，在必要证明文件下我们第一时间撤除。',
   'announcement' => '<div style="text-align: center;">
 <script id="LA-DATA-WIDGET" crossorigin="anonymous" charset="UTF-8" src="https://v6-widget.51.la/v6/这填写你的掩码ID/quote.js?theme=0&f=12&display=0,0,1,1,0,0,1,0"></script>
