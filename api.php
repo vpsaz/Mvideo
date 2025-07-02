e3ae82372da9e461ba4bb0840804a018edefc761
米诺 <1213235865@qq.com>
2025-07-03 06:06:55 +0800

    Update api.php

--- api.php
+++ api.php
@@ -46,23 +46,27 @@ if ($refererDomain === $currentDomain && !empty($titleParam)) {
     <meta name="description" content="<?php echo $conf['description']; ?>">
     <meta name="keywords" content="<?php echo $conf['keywords']; ?>">
     <link rel="shortcut icon" href="https://pic1.imgdb.cn/item/6812e03558cb8da5c8d5d3c3.png" type="image/x-icon">
-    <script src="https://unpkg.com/hls.js@1.4.8/dist/hls.min.js"></script>
-    <script src="https://unpkg.com/flv.js@1.6.2/dist/flv.min.js"></script>
-    <script src="https://unpkg.com/dashjs@4.7.1/dist/dash.all.min.js"></script>
-    <script src="https://unpkg.com/artplayer@5.2.3/dist/artplayer.js"></script>
-    <script src="https://unpkg.com/artplayer-plugin-ads@latest/dist/artplayer-plugin-ads.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/hls.js@1.4.8/dist/hls.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/flv.js@1.6.2/dist/flv.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/dashjs@4.7.1/dist/dash.all.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/artplayer@5.2.3/dist/artplayer.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/artplayer-plugin-ads@latest/dist/artplayer-plugin-ads.min.js"></script>
     <style>
-        body,
-        html {
+        html, body {
             width: 100%;
             height: 100%;
+            margin: 0;
             padding: 0;
-            margin: 0
+            overflow: hidden;
+            background-color: #000;
         }
 
         #video {
+            width: 100%;
             height: 100%;
-            width: 100%
+            position: fixed;
+            top: 0;
+            left: 0;
         }
     </style>
 </head>
